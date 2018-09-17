67e782c035d5ecd08dc0d2bf65f0cbdbe1e96a7b
Pawitra <5910110629@psu.ac.th>
2018-09-17 11:14:25 +0700

    การใช้ props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import WordCard from './WordCard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello {"World"}
+      <div>
+        <WordCard value="hello"/>
       </div>
     );
   }
